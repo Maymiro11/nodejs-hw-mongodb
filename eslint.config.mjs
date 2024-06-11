c906339c0c6aa11a560e0dd2ac009c047414572c
Maymiro11 <143181180+Maymiro11@users.noreply.github.com>
2024-06-11 15:25:14 +0200

    server-fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,20 +2,14 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  {
-    languageOptions: {
-      globals: globals.node,
-      parserOptions: {
-        ecmaVersion: 12,
-        sourceType: "module"
-      },
-    },
-  },
   pluginJs.configs.recommended,
   {
+    files: ["src/**/*.js"],
+    languageOptions: { globals: globals.node },
     rules: {
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"]
-    }
-  }
+      semi: "error",
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+  },
 ];
--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,11 +3,11 @@ import { env } from "../utils/env.js";
 
 export const initMongoConnection = async () => {
     try {
-      const user = env("MONGODB_USER");
-      const pwd = env("MONGODB_PASSWORD");
+      const user = env("MONGO_USER");
+      const pwd = env("MONGO_PASSWORD");
       const url = env("MONGODB_URL");
       const db = env("MONGODB_DB");
-  
+
       await mongoose.connect(
         `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
       );
