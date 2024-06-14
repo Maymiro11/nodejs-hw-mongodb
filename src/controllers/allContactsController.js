c5eabdd79e3b7210a42693e98cc90251368a515c
Maymiro11 <143181180+Maymiro11@users.noreply.github.com>
2024-06-14 13:51:15 +0200

    contacts-fix

--- src/controllers/allContactsController.js
+++ src/controllers/allContactsController.js
@@ -2,6 +2,7 @@ import { getAllContacts } from '../services/contacts.js';
 
 export const allContactsController = async (req, res) => {
   const contacts = await getAllContacts();
+  console.log(contacts);
   res.status(200).json({
     status: 200,
     message: contacts.length
